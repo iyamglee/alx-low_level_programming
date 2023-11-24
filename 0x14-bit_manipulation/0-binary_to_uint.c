17758854b93481a8d67b78bbfb63339326a29cb2
iyamglee <mrgleestudios@gmail.com>
2023-11-23 18:20:30 -0800

    0-100

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,20 +8,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-                int r;
-                        unsigned int decimal = 0;
+	int i;
+	unsigned int dec_val = 0;
 
-                                if (!b)
-                                                        return (0);
+	if (!b)
+		return (0);
 
-                                        for (r = 0; b[r]; r++)
-                                                        {
-                                                                                if (b[r] < '0' || b[r] > '1')
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		dec_val = 2 * dec_val + (b[i] - '0');
+	}
 
-      return (0);
-
-     decimal = 2 * decimal + (b[r] - '0');
-    }
-
-return (decimal);
+	return (dec_val);
 }
+
